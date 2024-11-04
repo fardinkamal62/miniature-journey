5eebf687afc03a3046edf3759e12c681868fcabc
Fardin Kamal <fardinkamal62@protonmail.ch>
2024-11-04 21:50:23 +0600

    changed the print text

--- main.java
+++ main.java
@@ -1,8 +1,5 @@
 class Main{
     public static void main(String[] arg){
-        System.out.println("Sinister Cypher");
-        System.out.println("Your Dark Guardian of Digital Secrets");
-
-        new PasswordStorage();
+        System.out.println("Hello Miniature People!");
     }
 }
